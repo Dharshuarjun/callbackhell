668eda7db1ec7beb61d5b975826f2717cb3de81a
Dharshuarjun <103558986+Dharshuarjun@users.noreply.github.com>
2022-05-27 16:36:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(function timer1(){
+    console.log("10");
+  setTimeout(function timer1(){
+    console.log("9");
+    setTimeout(function timer1(){
+        console.log("8");
+        setTimeout(function timer1(){
+            console.log("7");
+            setTimeout(function timer1(){
+                console.log("6");
+                setTimeout(function timer1(){
+                    console.log("5");
+                    setTimeout(function timer1(){
+                        console.log("4");
+                        setTimeout(function timer1(){
+                            console.log("3");
+                            setTimeout(function timer1(){
+                                console.log("2");
+                                setTimeout(function timer1(){
+                                    console.log("1");
+                                    setTimeout(function timer1(){
+                                        console.log("Happy Independence Day");
+                                    },2000);
+                                },2000);
+                            },2000);
+                        },2000);
+                    },2000);
+                },2000);
+            },2000);
+        },2000);
+    },2000);
+},2000);
+})
